434bf5825f353b00b25ab96691619f2dd1d2298b
Chaitali Narkhede <Chaitali_Narkhede@epam.com>
2021-10-12 18:59:09 +0530

    Initial Commit

---
+++ StudentService/Controllers/StudentController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private IEnumerable<Student> getStudentList()
+        {
+            List<Student> lst = new List<Student>()
+            {
+                new Student {StudentId=1,StudentName="Test1",Class="6th"},
+                new Student {StudentId=2,StudentName="Test2",Class="7th"},
+                new Student {StudentId=3,StudentName="Test3",Class="10th"},
+                new Student {StudentId=4,StudentName="Test4",Class="5th"}
+            };
+            return lst;
+        }
+
+        [HttpGet]
+        public IActionResult getStudentData()
+        {
+            var result = getStudentList();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult getStudentData(int id)
+        {
+            var result = getStudentList().Where(x => x.StudentId == id).FirstOrDefault();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult saveData([FromBody] Student studModel)
+        {
+            var lst = getStudentList().ToList();
+            lst.Add(studModel);
+            return Ok(lst);
+        }
+
+        [HttpPut]
+        public IActionResult updateData([FromBody] Student studModel)
+        {
+            var obj = getStudentList().Where(x => x.StudentId == studModel.StudentId).FirstOrDefault();
+            obj = studModel;
+            return Ok(obj);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteData(int id)
+        {
+            var obj = getStudentList().Where(x => x.StudentId != id).ToList();
+            return Ok(obj);
+        }
+    }
+}
---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Models;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private IEnumerable<User> getUserList()
+        {
+            List<User> lst = new List<User>()
+            {
+                new User {UserId=1,Name="Test1",Address="Pune"},
+                new User {UserId=2,Name="Test2",Address="Mumbai"},
+                new User {UserId=3,Name="Test3",Address="Pune"},
+                new User {UserId=4,Name="Test4",Address="Mumbai"}
+            };
+
+            return lst;
+        }
+
+        //[HttpGet("getData")]
+        //public IActionResult getData()
+        //{
+        //    List<User> lst = new List<User>()
+        //    {
+        //        new User {UserId=1,Name="Test1",Address="Pune"},
+        //        new User {UserId=2,Name="Test2",Address="Mumbai"},
+        //        new User {UserId=3,Name="Test3",Address="Pune"},
+        //        new User {UserId=4,Name="Test4",Address="Mumbai"}
+        //    };
+        //    return Ok(lst);
+        //}
+
+        //[HttpGet("getData/{id}")]
+        //public IActionResult getData(int id)
+        //{
+        //    List<User> lst = new List<User>()
+        //    {
+        //        new User {UserId=1,Name="Test1",Address="Pune"},
+        //        new User {UserId=2,Name="Test2",Address="Mumbai"},
+        //        new User {UserId=3,Name="Test3",Address="Pune"},
+        //        new User {UserId=4,Name="Test4",Address="Mumbai"}
+        //    };
+
+        //    var result = lst.Where(x => x.UserId == id).FirstOrDefault();
+        //    return Ok(result);
+        //}
+
+        [HttpGet]
+        public IActionResult getData()
+        {
+            var result = getUserList();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult getData(int id)
+        {
+            var result = getUserList().Where(x => x.UserId == id).FirstOrDefault();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult saveData([FromBody] User userModel)
+        {
+            var lst = getUserList().ToList();
+            lst.Add(userModel);
+            return Ok(lst);
+        }
+
+        [HttpPut]
+        public IActionResult updateData([FromBody] User userModel)
+        {
+            var obj = getUserList().Where(x => x.UserId == userModel.UserId).FirstOrDefault();
+            obj = userModel;
+            return Ok(obj);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteData(int id)
+        {
+            var obj = getUserList().Where(x => x.UserId != id).ToList();
+            return Ok(obj);
+        }
+    }
+}
